b5b8e38f1bc23e648ade56772cdff972436a4ff8
zpicy69 <aamsdoumani@gmail.com>
2024-02-17 14:01:00 -0700

    authentication added plus updated routes so that they're protected

--- budget-buddy/app/(main)/page.tsx
+++ budget-buddy/app/(main)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return(
     <div>
-      <h1 className="text-3xl text-red-500">Home</h1>
+      <h1>Home</h1>
+      <UserButton />
     </div>
   );
 }
---
+++ budget-buddy/components/test.tsx
@@ -0,0 +1,10 @@
+const Example = () => {
+    return (
+        <div className="h-full justify-content justify-center">
+            <h1>Example</h1>
+            <p>Example component</p>
+        </div>
+    );
+}
+ 
+export default Example;
