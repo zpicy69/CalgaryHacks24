c88235b62d961b9131adfd4e48e141d71ef50964
Abdalla <zpicy69@aamsdoumani@gmail.com>
2024-02-17 21:16:16 -0700

    landing navbar tweaked

--- budget-buddy/components/landing-navbar.tsx
+++ budget-buddy/components/landing-navbar.tsx
@@ -10,21 +10,25 @@ export const LandingNavbar = () => {
     return (
         <div className="flex flex-col items-center py-4 px-6 bg-[#111827] text-white">
             <nav className="w-full flex justify-between items-center">
+                {/* Learn More Link */}
                 <Link href="/learn-more">
                     <span className="text-sm uppercase cursor-pointer hover:underline">Learn More</span>
                 </Link>
 
+                {/* Placeholder for right-aligned content */}
                 <div className="text-right">
                     <span className="text-xs">Built By University Students, For University Students</span>
                 </div>
             </nav>
             <div className="text-center my-4">
+                {/* Logo and Title */}
                 <div className="flex justify-center items-center">
                     <div className="relative h-12 w-12 mr-3">
                         <Image src="/logo.png" layout="fill" alt="Logo" priority />
                     </div>
                     <h1 className="text-4xl font-bold">BudgetBuddy</h1>
                 </div>
+                {/* Tagline and Get Started Button */}
                 <p className="mt-2">
                     Simplify Budgeting - Stay On Top Of Your Finances
                 </p>
@@ -40,10 +44,6 @@ export const LandingNavbar = () => {
                     </button>
                 </Link>
             </div>
-
-            <div className="relative h-24 w-24">
-                <Image src="/image.png" layout="fill" alt="Chart Logo" priority />
-            </div>
-        </nav>
+        </div>
     );
 };
