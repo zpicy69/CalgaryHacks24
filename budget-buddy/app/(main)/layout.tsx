5ff5499fec3014be97d5fdd5a8b4037075a34321
Abdalla <zpicy69@aamsdoumani@gmail.com>
2024-02-17 23:28:33 -0700

    changed layout of landing page

--- budget-buddy/app/(main)/layout.tsx
+++ budget-buddy/app/(main)/layout.tsx
@@ -1,11 +1,18 @@
-const LandingLayout = ({ children }: {children: React.ReactNode;}) => {
-    return (
-        <main className="flex h-screen bg-[#080808] text-white overflow-auto">
-            <div className="m-auto w-full max-w-screen-xl px-4">
-                {children}
-            </div>
-        </main>
-    );
-}
+import Image from "next/image";
+import Link from "next/link";
 
-export default LandingLayout;
+const DashboardLayout = ({ children }: {children: React.ReactNode}) => {
+    return (
+        <div className="flex min-h-screen bg-[#111827] text-white">
+            <main className="flex-1 flex flex-col">
+                <header className="flex justify-between items-center p-6">
+                </header>
+                <div className="flex-1 p-6 overflow-auto">
+                    {children}
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default DashboardLayout;
