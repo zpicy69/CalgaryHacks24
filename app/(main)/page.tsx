b1e1e0f9d2ae53c5c4f96a7b629134891131869f
Abdalla <zpicy69@aamsdoumani@gmail.com>
2024-02-18 03:34:10 -0700

    P

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,10 @@
 import { LandingNavbar } from "@/components/landing-navbar";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return(
     <div className="h-full">
+      <UserButton afterSignOutUrl="/" />
       <LandingNavbar />
     </div>
   );
