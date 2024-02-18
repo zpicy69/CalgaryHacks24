288480fe312341dad2cc05bd4eb255df737d7042
Abdalla <zpicy69@aamsdoumani@gmail.com>
2024-02-18 03:15:21 -0700

    pluh

--- app/(dashboard)/(route)/budget/page.tsx
+++ app/(dashboard)/(route)/budget/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
--- app/(dashboard)/(route)/tfsa/page.tsx
+++ app/(dashboard)/(route)/tfsa/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
